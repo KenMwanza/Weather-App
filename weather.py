f3b970cb324b012dd281db348e4e9a28b16f6ab6
kennedy <mutemikennedy@gmail.com>
2016-08-22 17:08:56 +0300

    [weather.py] Format output
    Format output of function weather into columns

--- weather.py
+++ weather.py
@@ -1,26 +1,33 @@
 import urllib3
 import json
 
+from pprint import pprint
+
+BASEURL = 'http://api.openweathermap.org/data/2.5/weather'
+CONSUMER_KEY = '2bc3e79bb974a007818864813f53fd35'
+
 def weather(cities):
+
+    ''' Function to print weather data for various cities '''
 
     list_cities = []
     http = urllib3.PoolManager()
-    consumer_key = '2bc3e79bb974a007818864813f53fd35'
     units = 'metrics'
 
-    baseurl = 'http://api.openweathermap.org/data/2.5/weather'
-   
     for city in cities:
         r = http.request(
                 'GET',
-                baseurl,
+                BASEURL,
                 fields={
                     'q':city,
-                    'APPID': consumer_key,
+                    'APPID': CONSUMER_KEY,
                     'units': units
                 })
 
         list_cities.append(json.loads(r.data.decode('utf-8')))
-    for city in range(len(list_cities)):
-        pprint(list_cities[city]['main']) + " - " + list_cities[city]['name']
+    for city in list_cities:
+        pprint(str(city['name']).ljust(20))
+        print " - Temp: {0} Temp_min: {1} Temp_max: {2}".format(str(city['main']['temp']).ljust(10), str(city['main']['temp_min']).ljust(10), str(city['main']['temp_max']).ljust(10))
 
+if __name__ == '__main__':
+    weather(['Nairobi', 'Lagos', 'New York'])
